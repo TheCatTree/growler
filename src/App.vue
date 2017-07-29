1db975323eaab1227a94c83ee113e7c40f5975ec
TheCatTree <steven.c.hartley@gmail.com>
2017-07-29 22:45:30 +1200

    Got vuex working

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,32 @@
     <router-link v-bind:to="'/about'">About</router-link>
     <router-link v-bind:to="'/param'">Param Link</router-link>
     <router-link v-bind:to="'/weather'">Weather Link</router-link>
+    <p>{{ count }}</p>
+    <p>
+      <button @click="increment">+</button>
+      <button @click="decrement">-</button>
+    </p>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app'
+  name: 'app',
+
+  computed: {
+    count () {
+      return this.$store.state.count
+    }
+  },
+  methods: {
+    increment () {
+      return this.$store.commit('increment')
+    },
+    decrement () {
+      return this.$store.commit('decrement')
+    }
+  }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -3,13 +3,36 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
 
 Vue.config.productionTip = false
+
+const store = new Vuex.Store({
+  state: {
+    count: 0
+  },
+  mutations: {
+    increment: state => state.count++,
+    decrement: state => state.count--
+  }
+})
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store: store,
   template: '<App/>',
-  components: { App }
+  components: { App },
+  methods: {
+    increment () {
+      store.commit('increment')
+    },
+    decrement () {
+      store.commit('decrement')
+    }
+  }
 })
+
